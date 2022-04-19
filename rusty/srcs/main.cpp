a380ebccff7f99bb87b96f1120cf1c16d5056c1a
f0rkr <mashad@student.1337.ma>
2022-04-19 11:09:04 +0000

    Adding main file.

---
+++ rusty/srcs/main.cpp
@@ -0,0 +1,15 @@
+#include "main.hpp"
+
+/**
+ * @brief Main Routine function
+ * Parsing flags, arguments and starting the inker scanner
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int     main(int argc, char **argv) {
+    // TO-DO: Parsing command line arguments
+    
+    return (0);
+}
